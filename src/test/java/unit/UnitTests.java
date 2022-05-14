44f5fbef677d2ca338823b16d4452a3b40e26e6e
babycvla <babycvla@fel.cvut.cz>
2022-05-14 22:21:01 +0200

    feat(test) - prace na testech

--- src/test/java/unit/UnitTests.java
+++ src/test/java/unit/UnitTests.java
@@ -6,6 +6,11 @@ public class UnitTests {
     public void FormatTimeTest() {}
     public void PieceFromString() {}
     public void boardToString() {}
+    public void setMotionToPawns(ArrayList<IPiece> IPieces, IPiece chosen){}
+
+    // Mocked tests
+    public void putPiece(){}
+
     public void generateAllPossibleWays(int x, int y) {}
 
-}
+    public void Image getPieceImage(Square square, Board board) {}
